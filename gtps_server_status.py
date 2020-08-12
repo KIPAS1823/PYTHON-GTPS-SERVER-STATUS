bb07b6d04e9d38edea3c4cdf45dccb00653d53f6
KIPAS1823 <69453197+KIPAS1823@users.noreply.github.com>
2020-08-12 15:05:37 +0700

    Update gtps_server_status.py

--- gtps_server_status.py
+++ gtps_server_status.py
@@ -19,12 +19,16 @@ async def on_ready():
 async def s(ctx):
 	if ctx.author.id == YOUR ACCOUNT ID:
 		player = open('online.txt').readlines()
+		listworldss = len(os.listdir('worlds'))
+		listplayerss = len(os.listdir('players'))
 		tm = datetime.now()
 		tim = tm.strftime('%H:%M %p')
 		embed = discord.Embed(color=0x00ff00, title='SERVER-STATUS')
 		embed.set_thumbnail(url=ctx.guild.icon_url)
 		embed.add_field(name='STATUS:', value='UP')
 		embed.add_field(name='Players Online:', value=player[0])
+		embed.add_field(name='Total Players Create', value=listplayerss)
+		embed.add_field(name='Total Worlds Create', value=listworldss)
 		embed.set_footer(text='LAST UPDATE TODAY AT ' + tim + '')
 		text = await ctx.send(embed=embed)
 		while True:
@@ -36,7 +40,7 @@ async def s(ctx):
 			newembed.add_field(name='STATUS:', value='UP')
 			newembed.add_field(name='Players Online:', value=players[0])
 			newembed.add_field(name='Total Players Create', value=listplayers)
-			newembed.add_field(name='Total Worlds Create', value=listword)
+			newembed.add_field(name='Total Worlds Create', value=listworld)
 			newembed.set_footer(text='LAST UPDATE TODAY AT ' + tim + '')
 			#start editing
 			edit = threading.Thread(target = await text.edit(embed=newembed))
